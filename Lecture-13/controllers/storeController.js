eaccb3e56ef19fa478b47f8234a469d274eb7bca
Fazo01 <fg7829098@gmail.com>
2025-07-19 23:01:35 +0500

    Responsiveness issue

--- Lecture-13/controllers/storeController.js
+++ Lecture-13/controllers/storeController.js
@@ -1,10 +1,20 @@
 const Home = require("../models/home"); //Adding Module
 
+exports.getIndex = (req, res, next) => {//Adding module
+  Home.fetchAll((registeredHomes)=>
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb home",
+      currentPage: "index",
+    }) //Important to change in partial
+  );
+};
+
 exports.gethome = (req, res, next) => {//Adding module
   Home.fetchAll((registeredHomes)=>
     res.render("store/home-list", {
       registeredHomes: registeredHomes,
-      pageTitle: "airbnb home",
+      pageTitle: "Homes List",
       currentPage: "Home",
     }) //Important to change in partial
   );
@@ -14,7 +24,15 @@ exports.getBookings=(req, res, next) => {
     res.render("store/bookings", {
       pageTitle: "My bookings",
       currentPage: "bookings",
-    }) //Important to change in partial
+    })
 };
-
+exports.getFavouriteList=(req, res, next) => {
+    Home.fetchAll((registeredHomes)=>
+    res.render("store/favourite-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Favourite List",
+      currentPage: "favourite",
+    }) //Important to change in partial
+  );
+  }
 // exports.registeredHomes = registeredHomes;
--- Lecture-13/routes/storeRouter.js
+++ Lecture-13/routes/storeRouter.js
@@ -5,6 +5,8 @@ const storeRouter=express.Router()
 const Homecontroller=require("../controllers/storeController")//mvc
 
 storeRouter.get("/",Homecontroller.gethome)
+storeRouter.get("/index",Homecontroller.getIndex)
 storeRouter.get("/bookings",Homecontroller.getBookings)
+storeRouter.get("/favourites",Homecontroller.getFavouriteList)
 
 module.exports=storeRouter
